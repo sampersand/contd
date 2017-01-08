d8aace62ca52617def43b55212fa3e4d40ce27db
westerhack <westerhack@gmail.com>
2017-01-07 19:41:54 -0600

    Why add globals? why not just copy the vars each time? let's do that

--- objects/container.rb
+++ objects/container.rb
@@ -2,63 +2,74 @@ class Container
 
   # ----- Attributes ----- #
   attr_reader :stack
-  attr_reader :knowns
+  attr_reader :locals
+  attr_reader :globals
 
   # ----- Initializer ----- #
-  def initialize(stack: [], knowns: {})
-    @stack  = stack
-    @knowns = knowns
-    fail unless stack.respond_to?(:each)
-    fail unless stack.respond_to?(:pop)
-    fail unless stack.respond_to?(:push)
-    fail unless knowns.respond_to?(:[])
-    fail unless knowns.respond_to?(:[]=)
+  def initialize(stack: nil, locals: nil, globals: nil)
+    @stack  = stack || []
+    @locals = locals || {}
+    @globals = globals || {}
+    fail unless @stack.respond_to?(:each)
+    fail unless @stack.respond_to?(:pop)
+    fail unless @stack.respond_to?(:push)
+    fail unless @locals.respond_to?(:[])
+    fail unless @locals.respond_to?(:[]=)
+    fail unless @globals.respond_to?(:[])
+    fail unless @globals.respond_to?(:[]=)
+
   end
 
 
-  # ----- merging ----- #
-  
-  def merge_knowns(other)
-    # possibly dangerous with the cloning fun stuff :P
-    self.class.new(stack: @stack,
-                   knowns: @knowns.clone.update(other.knowns))
+  # ----- Indexing ----- #
+  def [](val)
+    @locals[val] || @globals[val]
+  end
+  def <<(val)
+    @stack << val
+  end
+  def pop
+    @stack.pop
   end
 
   # ----- Execution ----- #
-  def call!
-    result = Container.new       # DANGEROUS?
-    result.knowns.update(knowns) # DANGEROUS?
+  def call(result)
+    # result = Container.new       # DANGEROUS?
+    # result.locals.update(locals) # DANGEROUS?
 
     @stack.each do |token|
       case token
       when Keyword::Get
-        key = result.stack.pop
-        val = knowns[key]
+        key = result.pop
+        val = self[key]
         raise NameError, "Unknown token: #{key.inspect}" unless val
-        result.stack << val
+        result << val
       when Keyword::Call
-        args = result.stack.pop
-        func = result.stack.pop
+        args = result.pop
+        func = result.pop
         args = args.(result)  # DANGEROUS
         func.(args, result) # UNKNOWN IF THIS WORKS
       else
         fail "TODO: Keyeword #{token}" if token.is_a?(Keyword)
-        result.stack << token
+        result << token
       end
     end
     result
   end
 
-  def call(args)
-    merge_knowns(args).call!
+  def execute(args)
+    result = self.class.new(globals: @globals.clone
+
+                            )
+    puts result
+    #something funky might go on here with locals
+    exit
+    merge_globals(args).call!
   end
 
   # ----- Representation ----- #
   def to_s
-    return "()" if @stack.empty? && @knowns.empty?
-    return knowns_str if @stack.empty?
-    return stack_str if @knowns.empty?
-    "(#{stack_str}, #{knowns_str})"
+    "(#{stack_str}, #{locals_str}, #{globals_str})"
   end
 
   def inspect
@@ -68,19 +79,14 @@ class Container
   def stack_str
     "[#{@stack.join(', ')}]"
   end
-
-  def knowns_str
-    "{#{@knowns.collect{|k, v| "#{k}: #{v}"}.join(', ')}}"
+  def globals_str
+    globalz = globals.reject{ |_, v| v.is_a?(Proc)}
+    "{#{globalz.collect{|k, v| "#{k}: #{v}"}.join(', ')}}"
   end
 
-  def ai(options)
-    awesome_inspect(options).ai(options)
+  def locals_str
+    "{#{@locals.collect{|k, v| "#{k}: #{v}"}.join(', ')}}"
   end
-
-  def awesome_inspect(options)
-    @stack.collect{ |ele| ele.awesome_inspect(options) }
-  end
-
 end
 
 
@@ -98,19 +104,68 @@ if __FILE__ == $0
     Keyword::Call.new
   ])
 
-  args = Container.new(knowns: {
-    Identifier.new( :x ) => Identifier.new(3),
-    Identifier.new( :+ ) => proc{ |args, result|
-      right = args.stack.pop
-      left = args.stack.pop
-      result.stack << left.class.new(left.token + right.token)
+  args = Container.new(
+    locals:
+      {
+        Identifier.new( :x ) => Identifier.new(3),
       },
-  })
+    globals:
+      {
+        Identifier.new( :+ ) => proc{ |args, result|
+          right = args.stack.pop
+          left = args.stack.pop
+          result.stack << left.class.new(left.token + right.token)
+        },
+      }
+  )
 
   result = body.call(args)
 
-  require 'ap'
-  ap result, index: false
+  p result
 end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
