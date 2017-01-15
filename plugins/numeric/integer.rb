420577b1b3caf27ff5798ee73a95f92dc291ba51
westerhack <westerhack@gmail.com>
2017-01-14 21:06:49 -0600

    I should make the plugins modules not classes

---
+++ plugins/numeric/integer.rb
@@ -0,0 +1,40 @@
+module Numeric
+  class NumericPlugin
+    def next_token(iter)
+      return unless /\d/ =~ iter.peek
+      res = ''
+      res += iter.next until /[^.\d]/ =~ iter.peek
+      res
+    end
+
+    def handle_token(token, result, iter)
+      case token
+      when /\d+\.\d+/
+        result << token.to_f
+      when /\d+/
+        result << token.to_i
+      end
+    end
+  end
+
+end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- std/default_plugin.rb
+++ std/default_plugin.rb
@@ -1,39 +1,39 @@
-require_relative 'default_options'
+# require_relative 'default_options'
 
-module Std
+# module Std
 
-  class DefaultPlugin
-    attr_reader :options
+#   class DefaultPlugin
+#     attr_reader :options
 
-    def self.option_method(sym)
-      define_method(sym) do |*a, &b|
-        @options.each do |option|
-          next unless option.respond_to?(sym)
-          res = option.method(sym).call(*a, &b) and return res
-        end
-        nil
-      end
-    end
+#     def self.option_method(sym)
+#       define_method(sym) do |*a, &b|
+#         @options.each do |option|
+#           next unless option.respond_to?(sym)
+#           res = option.method(sym).call(*a, &b) and return res
+#         end
+#         nil
+#       end
+#     end
 
-    def next_token(iter)
-      iter.next
-    end
+#     def next_token(iter)
+#       iter.next
+#     end
 
-    def handle_token(token, result, iter)
-      case token
-      when whitespace? # do nothing
-      when start_comment?
-        nil until end_comment? === iter.next
-      when keyword?
-        result << (get_keyword token or raise "No keyword found for `#{token}`")
-      end
+#     def handle_token(token, result, iter)
+#       case token
+#       when whitespace? # do nothing
+#       when start_comment?
+#         nil until end_comment? === iter.next
+#       when keyword?
+#         result << (get_keyword token or raise "No keyword found for `#{token}`")
+#       end
 
-      true # this class can handle any token
+#       true # this class can handle any token
 
-    end
-  end
+#     end
+#   end
 
-end
+# end
 
 
 
