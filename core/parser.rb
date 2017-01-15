f3975c219042019849d3af8f6aae32a7e3f736ab
westerhack <westerhack@gmail.com>
2017-01-14 19:32:49 -0600

    Going to make these two things work swimmingly :)

--- core/parser.rb
+++ core/parser.rb
@@ -6,49 +6,18 @@ module Parser
 
   def parse(input, options)
     result = Container.new
+    iter = input.each
+    
   end
 
 end
 
 
 
-require_relative 'option'
+require_relative '../std/options'
 input = 'x = 3'
 
 
-option = Option.new
+option = Options.new
 res = Parser::parse(input, option)
 p res
-# class Object
-#   def imp
-#     case self
-#     when :** then 3
-#     when :* then 2
-#     when :+ then 1
-#     else 4
-#     end
-#   end
-# end
-
-# a = [0, :+, 1, :*, 2, :+, 3]
-# a = [0, :*, 1, :+, 2, :+, 3]
-
-# def next_priority(arr, sym)
-#   res = []
-#   until arr.empty?
-#     if (sym.imp) < arr.first.imp
-#       res << arr.shift
-#     else
-#       break
-#     end
-#   end
-#   res
-# end
-
-# p next_priority(a.clone, :+).join(' ')
-# p next_priority(a.clone, :*).join(' ')
-# p next_priority(a.clone, :**).join(' ')
-# p next_priority(a.clone, :/).join(' ')
-
-
-
