3c82ff3715af71bbe8c67e4456cf0843bfd76b9d
westerhack <westerhack@gmail.com>
2017-01-16 11:20:34 -0800

    merged. time to update all the plugins

--- core/parser.rb
+++ core/parser.rb
@@ -1,52 +1,72 @@
 require_relative 'container'
-require_relative 'char_stream'
 
 class Parser
-
   module DefaultPlugin
     module_function
-    def process_parser(parser)
-      result << parser.stream.next
+    def handle_next( parser )
+      result << parser.next
     end
   end
 
-  attr_reader :stream
   attr_reader :plugins
   attr_reader :result
 
   def initialize(input)
-    @stream = CharStream::from_str input
+    @chars = input.chars
     @plugins = [DefaultPlugin]
     @result = Container.new
   end
 
+  # --- Parsing --- #
   def add(plugin)
     @plugins.unshift plugin
     plugin.added(parser: self) if plugin.respond_to?(:added)
   end
 
   def run
-    process_parser until @stream.empty?
+    handle_next until empty?
     @result
   end
 
-  def process_parser
+  def handle_next
     @plugins.each do |plugin|
-      res = plugin.process_parser( self)
+      res = plugin.handle_next( self )
       return res if res
     end
   end
 
+  # --- Stream --- #
+  def next(amnt=1)
+    @chars.empty? and fail(EOFError, "Reached end of stream!")
+    @chars.shift(amnt).join
+  end
+
+  def feed(*vals)
+    @chars.unshift *vals
+  end
+
+  def empty?
+    @chars.empty?
+  end
+
+  def peek(amnt=1)
+    @chars.first(amnt).join
+  end
+
+  def to_a
+    @chars.clone
+  end
+
+  def next_while(&block)
+    res = []
+    res << self.next while block.call(self.peek)
+    res.join
+  end
+  
+  # --- Representaiton --- #
+  def to_s
+    @chars.to_s
+  end
+
 end
-
-
-
-
-
-
-
-
-
-
-
 
