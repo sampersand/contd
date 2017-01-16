0e06909e8a85d5689f59aaca817b4e1ef588ce04
westerhack <westerhack@gmail.com>
2017-01-16 10:13:05 -0800

    text_extend works

--- plugins/text/double_quote.rb
+++ plugins/text/double_quote.rb
@@ -1,16 +1,12 @@
-require_relative 'shared_functions'
+require_relative 'text_extend'
 
 module Text
   module DoubleQuote
-    
-    module_function
+    extend Text
 
-    QUOTE = '"'
-    ESCAPE = '\\'
-
-    def process_stream(**kwargs)
-      TextSharedFunctions::process_stream(plugin: self, **kwargs)
-    end
+    QUOTE = "\""
+    ESCAPE = "\\"
+    NEWLINE = "\n"
 
   end
 end
---
+++ plugins/text/text_extend.rb
@@ -0,0 +1,36 @@
+module Text
+  module ExtendedMethods
+
+    def process_stream(stream:, result:, **_)
+      fail "this only works for quotes and esapes of len 1" unless self::QUOTE.length == 1 &&
+                                                                   self::ESCAPE.length == 1
+      return unless stream.peek == self::QUOTE
+
+      start_quote = stream.next
+      end_quote = start_quote
+      body = ''
+
+      until (body += stream.next)[-1] == end_quote
+        
+        if body[-1] == self::ESCAPE
+          case stream.peek
+          when self::NEWLINE then stream.next #and ignore
+          else body += stream.next
+          end
+        end
+      end
+      result << start_quote + body
+      true
+    rescue stream.class::EOFError => e
+      raise stream.class::EOFError,
+            "Reached end of stream whilst looking for end of text (`#{self::QUOTE}`)"
+    end
+  end
+
+  module_function
+
+  def extended(other)
+    other.extend ExtendedMethods
+  end
+
+end
