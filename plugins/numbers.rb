85b615d721a80db10a5b6641b0c5fd101afc886e
westerhack <westerhack@gmail.com>
2017-01-16 11:34:36 -0800

    Removed Real; its going to be replaced with the operator 1.2

--- plugins/numbers.rb
+++ plugins/numbers.rb
@@ -1,6 +1,5 @@
 require_relative 'numbers/whole'
 require_relative 'numbers/radix'
-require_relative 'numbers/real'
 
 module Numbers
 
@@ -8,7 +7,6 @@ module Numbers
 
   def handle_next(parser)
     Radix.handle_next(parser) || 
-    Real.handle_next(parser) ||
     Whole.handle_next(parser)  
   end
 
